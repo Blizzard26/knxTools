8f8e093634ec2d63522fcdec9c4f664a338910f0
Andreas Lanz <andreas@familie-lanz.de>
2020-08-12 22:54:18 +0200

    ModelUtil für Verwendung in Velocity hinzugefügt

--- src/main/java/org/openhab/support/knx2openhab/velocity/VelocityProcessor.java
+++ src/main/java/org/openhab/support/knx2openhab/velocity/VelocityProcessor.java
@@ -7,14 +7,20 @@ import java.util.Properties;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.app.event.MethodExceptionEventHandler;
 import org.apache.velocity.context.Context;
 import org.apache.velocity.tools.ToolManager;
+import org.apache.velocity.util.introspection.Info;
 import org.knx.xml.KNX;
 import org.knx.xml.KnxProjectT.KnxInstallations.KnxInstallation;
 import org.openhab.support.knx2openhab.model.KNXThing;
+import org.openhab.support.knx2openhab.model.ModelUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VelocityProcessor
 {
+    protected static final Logger LOG = LoggerFactory.getLogger(VelocityProcessor.class);
 
     private final String template;
     private final File templatePath;
@@ -31,6 +37,7 @@ public class VelocityProcessor
         VelocityEngine velocityEngine = new VelocityEngine();
         Properties props = new Properties();
         props.put("resource.loader.file.path", this.templatePath.getAbsolutePath());
+        props.put("event_handler.method_exception.class", ExceptionHandler.class.getName());
         velocityEngine.init(props);
 
         ToolManager manager = new ToolManager(false, true);
@@ -43,8 +50,22 @@ public class VelocityProcessor
         context.put("things", things);
         context.put("knx", knx);
         context.put("installation", knxInstallation);
+        context.put("modelUtil", ModelUtil.class);
 
         t.merge(context, writer);
+
+    }
+
+    public static class ExceptionHandler implements MethodExceptionEventHandler
+    {
+
+        @Override
+        public Object methodException(final Context context, @SuppressWarnings("rawtypes") final Class claz,
+                final String method, final Exception e, final Info info)
+        {
+            LOG.error("Exception during template evaluation", e);
+            throw new RuntimeException(e);
+        }
 
     }
 
